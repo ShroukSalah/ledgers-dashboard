d0d6fe39b5a63713de285ffa27cda2262c394082
Shrouk Salah <shrouksalah707@outlook.com>
2026-03-08 02:50:45 +0200

    KpiCards

---
+++ src/components/dashboard/KpiCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    title: String,
+    value: String,
+    change: String
+})
+</script>
+
+<template>
+    <div class="bg-white rounded-xl p-3 border border-gray-100 shadow-xs hover:shadow-md transition">
+
+        <p class="text-sm  font-medium">
+            {{ title }}
+        </p>
+
+        <div class="mt-2  ">
+
+            <h3 class="text-xl font-semibold">
+                {{ value }}
+            </h3>
+
+            <span class="text-xs text-green-600 font-medium block text-end">
+                {{ change }}
+            </span>
+            <span class="text-xs text-gray-400 font-semibold">
+                vs previous month
+            </span>
+        </div>
+
+    </div>
+</template>
--- src/components/views/DashboardView.vue
+++ src/components/views/DashboardView.vue
@@ -1,14 +1,25 @@
 <script setup>
 import DashboardLayout from '../layout/DashboardLayout.vue'
+import KpiCard from '../dashboard/KpiCard.vue'
 </script>
 
 <template>
   <DashboardLayout>
 
     <div class="grid grid-cols-12 gap-6">
+      <div class="col-span-9">
+        <div class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-7 gap-4 mb-6">
 
-      <div class="col-span-12">
-        Dashboard content
+          <KpiCard title="Revenue" value="1400000" change="+4%" />
+          <KpiCard title="Gross Profit" value="1250000" change="+14%" />
+          <KpiCard title="Op.Expenses" value="600000" change="+34%" />
+          <KpiCard title="Net Income" value="650000" change="-25%" />
+          <KpiCard title="Cash in Bank" value="1700000" change="-25%" />
+          <KpiCard title="Burn Rate" value="750000" change="-15%" />
+          <KpiCard title="Runway" value="$40k" change="+4%" />
+
+        </div>
+
       </div>
 
     </div>
